4ced4f0e1898e786c8a28c504c96db72544f63a0
fabyday <rohjihyun95@gmail.com>
2026-05-14 05:01:10 +0900

    add image and View

---
+++ src/Renderer/View/PreferenceView/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import "../../style/index.css";
+
+const App = () => {
+    
+};
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
---
+++ src/Renderer/View/PreferenceView/PreferenceView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export function PreferenceView() {
+    
+
+  return <div>Preference View</div>;
+}
